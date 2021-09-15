5a5932117de2c9272f3cc624e27b8ecd82ee873c
Untermensch <AbsUntermensch@users.noreply.github.com>
2021-09-15 10:37:40 +0300

    Update Source.cpp

--- Lab2/Source.cpp
+++ Lab2/Source.cpp
@@ -14,30 +14,64 @@ int main()
 	Array<float> ar_fl(n);
 	while (true)
 	{
-		cout << "Select array type\n1.int\n2.float\n";
+		cout << "Select array type\n1.int\n2.float\n3.exit\n\n";
 		n = _getch();
-		switch (n)
+		switch(n)
 		{
 		case'1':
-			cout << "Enter array elements\n";
-			for (int i = 0; i < ar_int.Length(); i++)
-				cin >> ar_int[i];
-			cout << "Enter number: ";
-			cin >> n;
-			cout << "Sum = " << ar_int.Count(n) << endl;
-			cout << "Array elements\n";
-			Print(ar_int);
-			return 0;
+			while (true)
+			{
+				cout << "1.enter array elements\n2.count sum\n3.show array elements\n4.exit\n\n";
+				n = _getch();
+				switch (n)
+				{
+				case'1':
+					cout << "enter array elements : \n";
+					for (int i = 0; i < ar_int.Length(); i++)
+						cin >> ar_int[i];
+					break;
+				case '2':
+					cout << "Enter number: ";
+					cin >> n;
+					cout << "Sum = " << ar_int.Count(n) << endl;
+					break;
+				case '3':
+					cout << "Array elements\n";
+					Print(ar_int);
+					cout << endl;
+					break;
+				case '4':
+					exit(0);
+				}
+			}
 		case'2':
-			cout << "Enter array elements\n";
-			for (int i = 0; i < ar_fl.Length(); i++)
-				cin >> ar_fl[i];
-			cout << "Enter number: ";
-			cin >> n;
-			cout << "Sum = " << ar_fl.Count(n) << endl;
-			cout << "Array elements\n";
-			Print(ar_fl);
-			return 0;
+			while (true)
+			{
+				cout << "1.enter array elements\n2.count sum\n3.show array elements\n4.exit\n\n";
+				n = _getch();
+				switch (n)
+				{
+				case'1':
+					cout << "enter array elements : \n";
+					for (int i = 0; i < ar_fl.Length(); i++)
+						cin >> ar_fl[i];
+					break;
+				case '2':
+					cout << "Enter number: ";
+					cin >> n;
+					cout << "Sum = " << ar_fl.Count(n) << endl;
+					break;
+				case '3':
+					cout << "Array elements\n";
+					Print(ar_fl);
+					cout << endl;
+					break;
+				case '4':
+					exit(0);
+				}
+			}
+		case '3':
+			exit(0);
 		default:
 			break;
 		}
